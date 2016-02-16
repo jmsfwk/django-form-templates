07ca4c0489ebee7e9d9518958f1f7d1d44f0a368
James Fenwick <j.fenwick@me.com>
2016-02-16 11:59:10 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-form-templates',
+    packages=find_packages(),
+    install_requires=[],
+    version='0.1.0',
+    author='James Fenwick',
+    author_email='jfenwick@tecaz.com',
+    license='GPLv2',
+    classifiers=['Development Status :: 3 - Alpha',
+                 'Intended Audience::Developers',
+                 'Framework :: Django',
+                 'Framework :: Django :: 1.9',
+                 'License :: OSI Approved',
+                 'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+                 'Natural Language :: English',
+                 'Programming Language :: Python',],
+)
